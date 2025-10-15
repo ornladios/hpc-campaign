cb5f9321b7814b556a236a528174767ffb507aa2
Eric Suchyta <eric.d.suchyta@gmail.com>
2025-10-15 11:21:33 -0400

    More transparent syntax import in __main__.py (#37)

--- hpc_campaign/__main__.py
+++ hpc_campaign/__main__.py
@@ -1,13 +1,13 @@
 import sys
 import argparse
-
+import importlib
 
 
 def ArgParse():
     if ('--help' in sys.argv) and (sys.argv[1] == '--help'):
         add_help = True
     else:
-        add_help=False
+        add_help = False
     parser = argparse.ArgumentParser(add_help=add_help, prog="hpc_campaign")
 
     parser.add_argument(
@@ -33,14 +33,22 @@ def main():
     subcmd, args = ArgParse()
     prog = "hpc_campaign {0}".format(subcmd)
 
+    '''
     exec(
         'from .{0} import main as cmd'.format(subcmd),
         globals()
     )
 
     cmd(args=args, prog=prog)
+    '''
+
+    # Dynamically import the module using importlib.import_module()
+    aliased_module = importlib.import_module(".{0}".format(subcmd), package="hpc_campaign")
+
+    # Run main()
+    aliased_module.main(args=args, prog=prog)
 
 
 if __name__ == "__main__":
-    
+
     main()
